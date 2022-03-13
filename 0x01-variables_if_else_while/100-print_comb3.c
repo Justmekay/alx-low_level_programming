ddd93e2da116a729d8ef3496d0829d13f0dc24f9
Corlade <ogunwolekolade@yahoo.com>
2022-03-13 13:24:22 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,41 +1,37 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - entry point
  *
- * Return: Always 0 (Success/correct)
+ * Return: 0
  */
 
 int main(void)
-
 {
-int x;
-int y;
-int z;
+	int x;
+	int y;
+	int z;
 
-x = 0;
+	x  = 0;
 
-while (x < 100)
-{
-y = x / 10;
-z = x % 10;
+	while (x < 100)
+	{
+		y = x / 10;
+		z = x % 10;
 
-if (y < z)
-{
-putchar (y + '0');
-putchar (z + '0');
+		if (y < z)
+		{
+			putchar(y + '0');
+			putchar(z + '0');
 
-if (x < 89)
-{
-putchar (',');
-putchar (' ');
-}
+			if (x < 89)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
 
-}
-}
-
-x++;
-}
-putchar ('\n');
-
-return (0);
+		x++;
+	}
+	putchar('\n');
+	return (0);
 }
