d953cb213a5e0161066723101ea01d91bf1d7645
Corlade <ogunwolekolade@yahoo.com>
2022-03-16 16:29:56 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,6 +1,7 @@
 #include "main.h"
+
 /**
- * print_times_table - prints the n time table, starting with 0
+ * print_times_table - prints the n times table, starting with 0
  *
  * @n: int param
  */
