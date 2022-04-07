95298b31fc0fc6ebef9753f3b346b23c634489dd
Corlade <ogunwolekolade@yahoo.com>
2022-04-07 13:43:53 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,69 +1,49 @@
 #include "main.h"
-/**
- *_strlen - count array
- *@s: array of elements
- *Return: int i
- */
-int _strlen(char *s)
-{
-unsigned int i;
-
-i = 0;
-while (s[i] != '\0') /*Count charater of string*/
-{
-i++;
-}
-return (i);
-}
+#include <stdlib.h>
 
 /**
- *string_nconcat - concatenates two strings
- *@s1: string
- *@s2: string
- *@n: number bytes
- *Return: char dst
+ * string_nconcat - concatenates two strings.
+ * @s1: first string.
+ * @s2: second string.
+ * @n: amount of bytes.
+ *
+ * Return: pointer to the allocated memory.
+ * if malloc fails, status value is equal to 98.
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-char *dst;
-unsigned int i, count, j, size, size_1, size_2;
+	char *sout;
+	unsigned int ls1, ls2, lsout, i;
 
-if (s1 == NULL) /*If the array is empty*/
-s1 = "";
+	if (s1 == NULL)
+		s1 = "";
 
-if (s2 == NULL)
-s2 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-size_1 = _strlen(s1);
-size_2 = _strlen(s2);
+	for (ls1 = 0; s1[ls1] != '\0'; ls1++)
+		;
 
-if (n >= size_2)
-{
-count = size_2;
-}
-else
-{
-count = n;
-}
-/*count size total*/
-size = size_1 + count + 1;
-/*malloc*/
-dst = (char *) malloc(size * sizeof(char));
-if (dst == 0)
-{
-return (NULL);
-}
-/*Concantenate arrays*/
-{
-for (i = 0; *(s1 + i) != '\0'; i++)
-*(dst + i) = *(s1 + i);
-for (j = 0; j < count; j++)
-{
-*(dst + i) = *(s2 + j);
-i++;
-}
-*(dst + i) = '\0';
-}
-return (dst);
+	for (ls2 = 0; s2[ls2] != '\0'; ls2++)
+		;
 
+	if (n > ls2)
+		n = ls2;
+
+	lsout = ls1 + n;
+
+	sout = malloc(lsout + 1);
+
+	if (sout == NULL)
+		return (NULL);
+
+	for (i = 0; i < lsout; i++)
+		if (i < ls1)
+			sout[i] = s1[i];
+		else
+			sout[i] = s2[i - ls1];
+
+	sout[i] = '\0';
+
+	return (sout);
 }
